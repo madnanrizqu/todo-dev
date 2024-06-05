6af6dab27fab81186f979d4da5614e4069a327d6
Muhammad Adnan R <madnanrizqullah@gmail.com>
2024-06-05 15:21:32 +0300

    fix: search bar dissapear

--- src/App.tsx
+++ src/App.tsx
@@ -46,7 +46,7 @@ function App() {
       </header>
 
       <div
-        className={clsx("space-y-16 relative", {
+        className={clsx("space-y-4 relative", {
           "cursor-not-allowed": isProMode,
         })}
       >
@@ -83,118 +83,119 @@ function App() {
             </button>
           </label>
         </form>
-        {displayedTasks.length > 0 && (
-          <div className="space-y-4">
-            <form
-              onReset={handleResetSearch}
-              onSubmit={formSearch.handleSubmit((v) =>
-                handleSearchTask(v.searchQuery)
-              )}
-            >
-              <label
-                htmlFor="newTitle"
-                className="group input input-bordered flex items-center gap-2 transition-all hover:border-primary focus-within:border-primary"
+
+        <form
+          onReset={handleResetSearch}
+          onSubmit={formSearch.handleSubmit((v) =>
+            handleSearchTask(v.searchQuery)
+          )}
+        >
+          <label
+            htmlFor="newTitle"
+            className="group input input-bordered flex items-center gap-2 transition-all hover:border-primary focus-within:border-primary"
+          >
+            <input
+              id="searchTitle"
+              type="text"
+              className="grow"
+              placeholder="Search Task"
+              defaultValue={activeSearchQuery ?? ""}
+              {...formSearch.register("searchQuery")}
+            />
+            {activeSearchQuery && (
+              <button
+                className={clsx("opacity-0 hover:text-primary", {
+                  "group-focus-within:opacity-100": activeSearchQuery,
+                })}
+                type="reset"
               >
-                <input
-                  id="searchTitle"
-                  type="text"
-                  className="grow"
-                  placeholder="Search Task"
-                  defaultValue={activeSearchQuery ?? ""}
-                  {...formSearch.register("searchQuery")}
+                <RxCross2 />
+              </button>
+            )}
+            <button
+              className="opacity-0 group-focus-within:opacity-100 hover:text-primary"
+              type="submit"
+            >
+              <RxMagnifyingGlass />
+            </button>
+          </label>
+        </form>
+
+        <div className="divider"></div>
+
+        {displayedTasks.length > 0 && (
+          <ul className="space-y-4">
+            {displayedTasks.map((task) => (
+              <li key={task.id}>
+                <TaskComponent
+                  id={task.id}
+                  title={task.title}
+                  variant={mapStringToVariant(
+                    match(task.status)
+                      .with(TaskStatus.DONE, () => "done")
+                      .with(TaskStatus.NOT_DONE, () => "notDone")
+                      .with(TaskStatus.IN_EDIT, () => "inEdit")
+                      .otherwise(() => "notDone")
+                  )}
+                  onClickToggle={() => handleToggleTask(task.id)}
+                  onClickTriggerEdit={() => toggleTriggerUpdateTask(task.id)}
+                  onClickCancelEdit={() => toggleTriggerUpdateTask(task.id)}
+                  onSubmitEdit={(value) => handleUpdateTask(value, task.id)}
+                  onClickDelete={() => handleDeleteTask(task.id)}
+                  hasAddBtn
+                  onClickAdd={() => onCreateSubTask(task.id)}
                 />
-                {activeSearchQuery && (
-                  <button
-                    className={clsx("opacity-0 hover:text-primary", {
-                      "group-focus-within:opacity-100": activeSearchQuery,
-                    })}
-                    type="reset"
-                  >
-                    <RxCross2 />
-                  </button>
-                )}
-                <button
-                  className="opacity-0 group-focus-within:opacity-100 hover:text-primary"
-                  type="submit"
-                >
-                  <RxMagnifyingGlass />
-                </button>
-              </label>
-            </form>
-            <div className="divider"></div>
-            <ul className="space-y-4">
-              {displayedTasks.map((task) => (
-                <li key={task.id}>
-                  <TaskComponent
-                    id={task.id}
-                    title={task.title}
-                    variant={mapStringToVariant(
-                      match(task.status)
-                        .with(TaskStatus.DONE, () => "done")
-                        .with(TaskStatus.NOT_DONE, () => "notDone")
-                        .with(TaskStatus.IN_EDIT, () => "inEdit")
-                        .otherwise(() => "notDone")
-                    )}
-                    onClickToggle={() => handleToggleTask(task.id)}
-                    onClickTriggerEdit={() => toggleTriggerUpdateTask(task.id)}
-                    onClickCancelEdit={() => toggleTriggerUpdateTask(task.id)}
-                    onSubmitEdit={(value) => handleUpdateTask(value, task.id)}
-                    onClickDelete={() => handleDeleteTask(task.id)}
-                    hasAddBtn
-                    onClickAdd={() => onCreateSubTask(task.id)}
-                  />
-                  <ul className="ml-10">
-                    {task?.subTasks?.map((subTask) => (
-                      <li key={subTask.id}>
-                        <TaskComponent
-                          id={subTask.id}
-                          title={subTask.title}
-                          variant={mapStringToVariant(
-                            match(subTask.status)
-                              .with(TaskStatus.DONE, () => "done")
-                              .with(TaskStatus.NOT_DONE, () => "notDone")
-                              .with(TaskStatus.IN_EDIT, () => "inEdit")
-                              .otherwise(() => "notDone")
-                          )}
-                          onClickTriggerEdit={() =>
-                            toggleTriggerUpdateTask(subTask.id, task.id)
-                          }
-                          onClickCancelEdit={() =>
-                            toggleTriggerUpdateTask(subTask.id, task.id)
-                          }
-                          onSubmitEdit={(value) =>
-                            handleUpdateTask(value, subTask.id, task.id)
-                          }
-                          onClickToggle={() =>
-                            handleToggleTask(subTask.id, task.id)
-                          }
-                          onClickDelete={() =>
-                            handleDeleteTask(subTask.id, task.id)
-                          }
-                        />
-                      </li>
-                    ))}
-                    <li
-                      className={clsx({
-                        "opacity-0": parentTaskIdForCreate !== task.id,
-                        "h-0": parentTaskIdForCreate !== task.id,
-                        "w-0": parentTaskIdForCreate !== task.id,
-                      })}
-                      key={`task-${task.id}-sub-task-form`}
-                    >
+                <ul className="ml-10">
+                  {task?.subTasks?.map((subTask) => (
+                    <li key={subTask.id}>
                       <TaskComponent
-                        disableKeyboardTab={parentTaskIdForCreate !== task.id}
-                        id={`task-${task.id}-sub-task-form`}
-                        variant="inCreate"
-                        onSubmitEdit={handleCreateTask}
-                        onClickCancelEdit={onCancelCreateSubTask}
+                        id={subTask.id}
+                        title={subTask.title}
+                        variant={mapStringToVariant(
+                          match(subTask.status)
+                            .with(TaskStatus.DONE, () => "done")
+                            .with(TaskStatus.NOT_DONE, () => "notDone")
+                            .with(TaskStatus.IN_EDIT, () => "inEdit")
+                            .otherwise(() => "notDone")
+                        )}
+                        onClickTriggerEdit={() =>
+                          toggleTriggerUpdateTask(subTask.id, task.id)
+                        }
+                        onClickCancelEdit={() =>
+                          toggleTriggerUpdateTask(subTask.id, task.id)
+                        }
+                        onSubmitEdit={(value) =>
+                          handleUpdateTask(value, subTask.id, task.id)
+                        }
+                        onClickToggle={() =>
+                          handleToggleTask(subTask.id, task.id)
+                        }
+                        onClickDelete={() =>
+                          handleDeleteTask(subTask.id, task.id)
+                        }
                       />
                     </li>
-                  </ul>
-                </li>
-              ))}
-            </ul>
-          </div>
+                  ))}
+                  <li
+                    className={clsx({
+                      "opacity-0": parentTaskIdForCreate !== task.id,
+                      "h-0": parentTaskIdForCreate !== task.id,
+                      "w-0": parentTaskIdForCreate !== task.id,
+                    })}
+                    key={`task-${task.id}-sub-task-form`}
+                  >
+                    <TaskComponent
+                      disableKeyboardTab={parentTaskIdForCreate !== task.id}
+                      id={`task-${task.id}-sub-task-form`}
+                      variant="inCreate"
+                      onSubmitEdit={handleCreateTask}
+                      onClickCancelEdit={onCancelCreateSubTask}
+                    />
+                  </li>
+                </ul>
+              </li>
+            ))}
+          </ul>
         )}
       </div>
     </main>
